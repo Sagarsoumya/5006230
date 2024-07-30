bd38e4890821737ff1d8dea6286ee78ee0541ff5
Soumya Sagar Rath <sagarsoumyarath07@gmail.com>
2024-07-31 01:55:26 +0530

    Exercise7

---
+++ WEEK_1/Exercise7/FinancialForecasting.java
@@ -0,0 +1,22 @@
+package WEEK_1.Exercise7;
+
+public class FinancialForecasting {
+    public static double calculateFutureValue(double presentValue, double growthRate, int years) {
+        
+        if (years == 0) {
+            return presentValue;
+        }
+        
+        double futureValue = calculateFutureValue(presentValue, growthRate, years - 1);
+        return futureValue * (1 + growthRate);
+    }
+
+    public static void main(String[] args) {
+        double presentValue = 1000; 
+        double growthRate = 0.05;   
+        int years = 10;            
+
+        double futureValue = calculateFutureValue(presentValue, growthRate, years);
+        System.out.println("Future Value after " + years + " years: " + futureValue);
+    }
+}
