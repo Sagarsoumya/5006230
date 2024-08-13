796707be4d17a6b368177fc660c82ee7ef08a0c5
Soumya Sagar Rath <sagarsoumyarath07@gmail.com>
2024-08-13 21:54:06 +0530

    Exercise 3

---
+++ WEEK_3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeManagementSystem.repository;
+
+import com.example.EmployeeManagementSystem.entity.Department;
+import com.example.EmployeeManagementSystem.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByName(String name);
+    Employee findByEmail(String email);
+    List<Employee> findByDepartment(Department department);
+}
