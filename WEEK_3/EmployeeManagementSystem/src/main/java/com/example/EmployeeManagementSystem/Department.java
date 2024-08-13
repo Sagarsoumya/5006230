a9e7ff5d195be83950fb7e62bc3f7578b25a743c
Soumya Sagar Rath <sagarsoumyarath07@gmail.com>
2024-08-13 21:43:14 +0530

    Exercise2

---
+++ WEEK_3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/Department.java
@@ -0,0 +1,18 @@
+package com.example.EmployeeManagementSystem;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy ="department")
+    private List<Employee> employees;
+}
