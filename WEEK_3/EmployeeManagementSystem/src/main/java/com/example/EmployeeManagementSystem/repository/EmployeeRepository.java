38a236596b245550047a9ff20f780aae201f8ecb
Soumya Sagar Rath <sagarsoumyarath07@gmail.com>
2024-08-14 16:17:14 +0530

    Exercise 7

--- WEEK_3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
+++ WEEK_3/EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepository.java
@@ -1,21 +1,18 @@
 package com.example.EmployeeManagementSystem.repository;
 
-import com.example.EmployeeManagementSystem.entity.Department;
 import com.example.EmployeeManagementSystem.entity.Employee;
-import org.hibernate.query.Page;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.PutMapping;
-
-import java.awt.print.Pageable;
-import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Page<Employee> findByName(String name,Pageable pageable);
+    Page<Employee> findByName(String name, Pageable pageable);
 
-    @Query("SELECT e FROM Employee e WHERE e.email =:email")
+    @Query("SELECT e FROM Employee e WHERE e.email = :email")
     Employee findByEmail(@Param("email") String email);
+
     @Query("SELECT e FROM Employee e WHERE e.department.name = :deptName")
     Page<Employee> findByDepartmentName(@Param("deptName") String deptName, Pageable pageable);
 }
